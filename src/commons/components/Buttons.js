8ea8ae12d67c0d77c324833ffb761b1e00d6debc
youngsan01 <intokys@gmail.com>
2024-04-16 17:52:21 +0900

    공통 컴포넌트 - 메인 제목, 버튼, 입력박스, 회원가입 페이지 구성 중

---
+++ src/commons/components/Buttons.js
@@ -0,0 +1,35 @@
+import styled, { css } from "styled-components";
+import { buttonColor } from "../../styles/color";
+import fontSize from "../../styles/fontSize";
+const { big } = fontSize;
+
+export const BigButton = styled.button`
+  font-size: ${big};
+  height: 45px;
+  width: 100%;
+  border-radius: 3px;
+  cursor: pointer;
+
+  ${({ color }) =>
+    buttonColor[color] &&
+    css`
+      background: ${buttonColor[color][0]};
+      color: ${buttonColor[color][1]};
+      border: 1px solid ${buttonColor[color][2]};
+    `}
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  width: ${({ width }) => (width ? `${width}px` : "100%")};
+  margin: 20px auto;
+
+  button {
+    width: 0;
+    flex-grow: 1;
+  }
+
+  button + button {
+    margin-left: 5px;
+  }
+`;
--- src/member/components/JoinForm.js
+++ src/member/components/JoinForm.js
@@ -1,8 +1,57 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { styled } from "styled-components";
+import { FaCheckSquare, FaRegCheckSquare } from "react-icons/fa";
+
+import { BigButton, ButtonGroup } from "../../commons/components/Buttons";
+import InputBox from "../../commons/components/InputBox";
+
+const FormBox = styled.form``;
 
 const JoinForm = () => {
-    return <h1>회원가입 양식...</h1>;
+  const { t } = useTranslation();
+  return (
+    <FormBox autoComplete="off">
+      <dl>
+        <dt>{t("이메일")}</dt>
+        <dd>
+          <InputBox type="text" />
+        </dd>
+      </dl>
+      <dl>
+        <dt>{t("비밀번호")}</dt>
+        <dd>
+          <InputBox type="password" />
+        </dd>
+      </dl>
+      <dl>
+        <dt>{t("비밀번호_확인")}</dt>
+        <dd>
+          <InputBox type="password" />
+        </dd>
+      </dl>
+      <dl>
+        <dt>{t("회원명")}</dt>
+        <dd>
+          <InputBox type="text" />
+        </dd>
+      </dl>
+      <dl>
+        <div className="terms-agree">
+          <FaCheckSquare /> {t("회원가입_약관에_동의합니다.")}
+        </div>
+      </dl>
+
+      <ButtonGroup width={450}>
+        <BigButton type="reset" color="light">
+          {t("다시입력")}
+        </BigButton>
+        <BigButton type="submit" color="dark">
+          {t("가입하기")}
+        </BigButton>
+      </ButtonGroup>
+    </FormBox>
+  );
 };
 
 export default React.memo(JoinForm);
-
